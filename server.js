018dd3077baf23d57a3997c6d6892584cb6f8013
kartikjn <kj@rapidinnovation.dev>
2022-11-24 14:39:13 +0530

    added keys

--- server.js
+++ server.js
@@ -34,13 +34,13 @@ function notificationReceived(req) {
 // add an address to a notification in Alchemy
 async function addAddress(new_address) {
   console.log("adding address " + new_address);
-  const body = { webhook_id: <webhook-id>, addresses_to_add: [new_address], addresses_to_remove: [] };
+  const body = { webhook_id: wh_coxv4xu36muaa4f4, addresses_to_add: [new_address], addresses_to_remove: [] };
   try {
     fetch('https://dashboard.alchemyapi.io/api/update-webhook-addresses', {
       method: 'PATCH',
       body: JSON.stringify(body),
       headers: { 'Content-Type': 'application/json' },
-      headers: { 'X-Alchemy-Token': <Alchemy-auth-key>}
+      headers: { 'X-Alchemy-Token': DFcVhH7ttQCKGDYk33jiGM7lYCdiltQ1}
     })
       .then(res => res.json())
       .then(json => console.log("Successfully added address:", json))
